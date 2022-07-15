01c0dc93cdcdf9d6bbfc68f1fe0a9e761dd90df9
Abhishek Gill <akumar074@gmail.com>
2022-07-15 10:26:58 +0530

    xref-validator test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Validate xref
-if [[ $(npx antora --generator @antora/xref-validator antora-playbook-author.yml) ]]; then
-    npx antora --generator @antora/xref-validator antora-playbook-author.yml
-    exit 1
+output=$(npx antora --generator @antora/xref-validator antora-playbook-author.yml)
+if [[ -n $output ]];
+then
+  echo $output
+  exit 1
 else
-    echo "xref validated successfully"
+  echo "xref validated successfully"
 fi
